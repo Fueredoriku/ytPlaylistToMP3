e0f4d93795b11682dc23d680d587bb2f76121107
Fredrik <fredrlei@stud.ntnu.no>
2021-09-05 01:49:37 +0200

    Added ability to get videoURL and paste into downloading site.

--- ytList2mp3.py
+++ ytList2mp3.py
@@ -9,3 +9,23 @@ driver.get('https://www.youtube.com/playlist?list=PLBgeu_-TzvHfABlVOp3BD-ZozTyz3
 #Accepts cookies for this browser instance
 cookieAllow = driver.find_element_by_xpath('//*[@id="yDmH0d"]/c-wiz/div/div/div/div[2]/div[1]/div[4]/form/div[1]/div/button')
 cookieAllow.click()
+
+#Starts Playlist
+playlistStart = driver.find_element_by_xpath('//*[@id="thumbnail"]')
+playlistStart.click()
+
+#Gets link of first video in list
+lastLink = driver.current_url
+
+#Goes to download-site
+driver.get('https://ytmp3.cc/en16/')
+
+#Fixes required inputs
+downloadField = driver.find_element_by_xpath('//*[@id="input"]')
+downloadField.send_keys(lastLink)
+
+convertButton = driver.find_element_by_xpath('//*[@id="submit"]')
+convertButton.click()
+
+downloadButton = driver.find_element_by_xpath('//*[@id="buttons"]/a[1]')
+downloadButton.click()
